c3f23960fa0973ff5613866a1cabaf2f732f1dc8
justinbatchelor <justinrossbatchelor@gmail.com>
2024-06-16 02:14:48 -0400

    updating testing files and assisted_installer

--- src/redhat_assisted_installer/lib/schemas.py
+++ src/redhat_assisted_installer/lib/schemas.py
@@ -1,4 +1,5 @@
 import os
+
 class ClusterCreateParams:
     def __init__(self, 
                  name: str, 
@@ -6,22 +7,22 @@ class ClusterCreateParams:
                  pull_secret: str = os.environ.get("REDHAT_PULL_SECRET"),
                  additional_ntp_source: str = None,
                  base_dns_domain: str = None,
-                 cluster_network_cidr: str = "10.128.0.0/14",
-                 cluster_network_host_prefix: int = 23,
-                 cpu_architecture: str = "x86_64",
-                 high_availability_mode: str = "None",
+                 cluster_network_cidr: str = None,
+                 cluster_network_host_prefix: int = None,
+                 cpu_architecture: str = None,
+                 high_availability_mode: str = None,
                  http_proxy: str = None,
                  https_proxy: str = None,
-                 hyperthreading: str = "all",
+                 hyperthreading: str = None,
                  network_type: str = None,
                  no_proxy: str = None,
                  ocp_release_image: str = None,
-                 schedulable_masters: int = False,
-                 service_network_cidr: str = "172.30.0.0/16",
+                 schedulable_masters: int = None,
+                 service_network_cidr: str = None,
                  ssh_public_key: str = None,
                  tags: str = None,
-                 user_managed_networking: int = False,
-                 vip_dhcp_allocation: int = False,
+                 user_managed_networking: int = None,
+                 vip_dhcp_allocation: int = None,
                 ):
         self.name = name
         self.openshift_version = openshift_version
@@ -46,29 +47,7 @@ class ClusterCreateParams:
         self.vip_dhcp_allocation = vip_dhcp_allocation
 
     def to_dict(self):
-        return {
-            "name": self.name,
-            "openshift_version": self.openshift_version,
-            "pull_secret": self.pull_secret,
-            "additional_ntp_source": self.additional_ntp_source,
-            "base_dns_domain": self.base_dns_domain,
-            "cluster_network_cidr": self.cluster_network_cidr,
-            "cluster_network_host_prefix": self.cluster_network_host_prefix,
-            "cpu_architecture": self.cpu_architecture,
-            "high_availability_mode": self.high_availability_mode,
-            "http_proxy": self.http_proxy,
-            "https_proxy": self.https_proxy,
-            "hyperthreading": self.hyperthreading,
-            "network_type": self.network_type,
-            "no_proxy": self.no_proxy,
-            "ocp_release_image": self.ocp_release_image,
-            "schedulable_masters": self.schedulable_masters,
-            "service_network_cidr": self.service_network_cidr,
-            "ssh_public_key": self.ssh_public_key,
-            "tags": self.tags,
-            "user_managed_networking": self.user_managed_networking,
-            "vip_dhcp_allocation": self.vip_dhcp_allocation
-        }
+        return {k: v for k, v in self.__dict__.items() if v is not None}
 
 
 class InfraEnvCreateParams:
@@ -78,7 +57,7 @@ class InfraEnvCreateParams:
                  additional_ntp_sources: str = None,
                  additional_trust_bundle: str = None,
                  cluster_id: str = None,
-                 cpu_architecture: str = "x86_64",
+                 cpu_architecture: str = None,
                  ignition_config_override: str = None,
                  openshift_version: str = None,
                  ssh_authorized_key: str = None,
@@ -94,17 +73,7 @@ class InfraEnvCreateParams:
         self.ssh_authorized_key = ssh_authorized_key
 
     def to_dict(self):
-        return {
-            "name": self.name,
-            "pull_secret": self.pull_secret,
-            "additional_ntp_sources": self.additional_ntp_sources,
-            "additional_trust_bundle": self.additional_trust_bundle,
-            "cluster_id": self.cluster_id,
-            "cpu_architecture": self.cpu_architecture,
-            "ignition_config_override": self.ignition_config_override,
-            "openshift_version": self.openshift_version,
-            "ssh_authorized_key": self.ssh_authorized_key,
-        }
+        return {k: v for k, v in self.__dict__.items() if v is not None}
 
 
 class ClusterUpdateParams:
@@ -148,26 +117,7 @@ class ClusterUpdateParams:
         self.vip_dhcp_allocation = vip_dhcp_allocation
 
     def to_dict(self):
-        return {
-            "additional_ntp_source": self.additional_ntp_source,
-            "api_vip_dns_name": self.api_vip_dns_name,
-            "base_dns_domain": self.base_dns_domain,
-            "cluster_network_cidr": self.cluster_network_cidr,
-            "cluster_network_host_prefix": self.cluster_network_host_prefix,
-            "http_proxy": self.http_proxy,
-            "https_proxy": self.https_proxy,
-            "hyperthreading": self.hyperthreading,
-            "name": self.name,
-            "network_type": self.network_type,
-            "no_proxy": self.no_proxy,
-            "pull_secret": self.pull_secret,
-            "schedulable_masters": self.schedulable_masters,
-            "service_network_cidr": self.service_network_cidr,
-            "ssh_public_key": self.ssh_public_key,
-            "tags": self.tags,
-            "user_managed_networking": self.user_managed_networking,
-            "vip_dhcp_allocation": self.vip_dhcp_allocation
-        }
+        return {k: v for k, v in self.__dict__.items() if v is not None}
     
 class InfraEnvUpdateParams:
     def __init__(self,
@@ -186,14 +136,7 @@ class InfraEnvUpdateParams:
         self.ssh_authorized_key = ssh_authorized_key
 
     def to_dict(self):
-        return {
-            "additional_ntp_sources": self.additional_ntp_sources,
-            "additional_trust_bundle": self.additional_trust_bundle,
-            "ignition_config_override": self.ignition_config_override,
-            "image_type": self.image_type,
-            "pull_secret": self.pull_secret,
-            "ssh_authorized_key": self.ssh_authorized_key,
-        }
+        return {k: v for k, v in self.__dict__.items() if v is not None}
     
 class HostCreateParams:
     def __init__(self, host_id: str, discovery_agent_version: str = None):
@@ -201,16 +144,11 @@ class HostCreateParams:
         self.discovery_agent_version = discovery_agent_version
 
     def to_dict(self):
-        return {
-            "host_id": self.host_id,
-            "discovery_agent_version": self.discovery_agent_version
-        }
+        return {k: v for k, v in self.__dict__.items() if v is not None}
     
 class BindHostParams:
     def __init__(self, cluster_id: str):
         self.cluster_id = cluster_id
 
     def to_dict(self):
-        return {
-            "cluster_id": self.cluster_id
-        }
+        return {k: v for k, v in self.__dict__.items() if v is not None}
